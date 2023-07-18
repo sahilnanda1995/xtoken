189e977908a01d0ada9fbfb238291ca0cd94d2d5
Sahil <sahilnanda1995@gmail.com>
2023-07-18 21:39:47 +0530

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    node: true, // Add this line
+  },
+  root: true,
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
+};
